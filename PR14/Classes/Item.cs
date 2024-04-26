aa72b29f6b197e458c9bfb65ad01a089c93cd223
Абду Дарвиш <abdu.dar05@mail.ru>
2024-04-27 01:54:35 +0500

    Разработанный класс. Darwish

---
+++ PR14/Classes/Item.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PR14.Classes
+{
+    public class Item
+    {
+        public string name { get; set; }
+        public int price { get; set; }
+        public string src { get; set; }
+
+        public int activeSize = 0;
+
+        public Item(string name, int price, string src)
+        {
+            this.name = name;
+            this.price = price;
+            this.src = src;
+        }
+    }
+}
